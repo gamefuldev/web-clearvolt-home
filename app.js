ea18dfbd1bd28aa9cc76f1928270f78a65ee3c54
Jared Rigby <talk@jaredrigby.co.uk>
2024-11-19 12:51:26 +0000

    Created responsive footer

---
+++ app.js
@@ -0,0 +1,14 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        darkgreen: '#0C9C44',
+        lightgreen: '#0A86A6',
+        turquoise: '#00A599',
+        darkgrey: '#353535'
+      }
+    }
+  }
+}
+
+const { animate, scroll } = Motion
